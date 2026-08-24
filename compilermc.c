7dc443d3df5f3f16b9fcd8588b82f36d33ae0ea6
falseinfo <norrmanmalte@gmail.com>
2026-08-24 21:02:49 +0200

    Refactor parser_microc function and fix errors

--- compilermc.c
+++ compilermc.c
@@ -1,85 +1,72 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 int MODE_X86_64 = 0;
 
 void parser_microc(const char **src) {
-  const char *p = *src;
-  while ((*p == '\n') || (*p == '\t') || (*p == ' ')){
-    p++;
-  }
-  if (strncmp(p, "head", 4) == 0) {
-    p += 4;
-
-    if (*p == '(') {
-      p++;
-      printf("opened head block\n");
-
-      if (strncmp(p, "asm-x86-64", 10) == 0) {
-        MODE_X86_64 = 1;
-        printf("-> enabled x86_64 mode\n");
-        p += 10;
-      }
-
-      while (*p != ')' && *p != '\0') {
-        putchar(*p);
+    const char *p = *src;
+    while ((*p == '\n') || (*p == '\t') || (*p == ' ')) {
         p++;
-      }
-
-      if (*p == ')') {  
-        printf("\nhead block closed\n");
-        p++;
-      }
-      while ((*p == '\n') || (*p == '\t') || (*p == ' ')) {
-        p++;
-      }
-      if (strncmp(p, "(asmb)", 6) == 0) {
-        p += 6;
-        printf("asmb found and opened\n");
-            while ((*p == '\n') || (*p == '\t') || (*p == ' ')) {
-              p++;
-            }
-          if (*p == '{') {
-            p++;
-            while (*p != '}' && *p != '\0') {
-              putchar(*p);
-              p++;
-            }
-            if (*p == '}') {
-              p++;
-            }
-            while ((*p == '\n') || (*p == '\t') || (*p == ' ')) {
-              p++;
-            }
-            if (strncmp(p, "(asme)", 6) == 0) {
-              p += 6;
-              printf("asme found and closed");
-            } else {
-              printf("error (asme) not found\n");
-            }
-            
-        }
-      }
-    } else {
-      printf("\nerror missing start of head block \"(\"\n");
     }
-  } else {
-    printf("\nerror missing head block\n");
-  }
-  *src = p;
-}
+    if  (strncmp(p, "head", 4) == 0) {
+        p += 4;
 
-int main() {
-  FILE *file = fopen("test.mc", "r");
-  if (file = NULL) {
-    printf("error: could not open test.mc");
-    return 1;
-  }
-  char *buffer = malloc(46 + 1);
-  fread(buffer, 1, 46, file);
-  buffer[46] = '\0';
-  fclose(file);
-  free(buffer);
-  return 0;
+        if (*p == '(') {
+            p++;
+
+            if (strncmp(p, "asm-x86-64", 10) == 0) {
+                MODE_X86_64 = 1;
+                p += 10;
+            
+            }
+            while (*p != ')' && *p != '\0') {
+                putchar(*p);
+                p++;
+            } 
+            if (*p == ')') {
+                p++;
+            } else {
+                printf("error: end of head block not found")
+            }
+            while (*p != '{' && *p != '\0') {
+                putchar(*p);
+                p++;
+            }
+            while ((*p == '\n') || (*p == '\t') || (*p == ' ')) {
+                p++;
+            }
+            if (strncmp(p, "(asmb)", 6) == 0) {
+                p += 6;
+                while ((*p == '\n') || (*p == '\t') || (*p == ' ')) {
+                    p++;
+                }
+                if (*p == '{') {
+                    p++;
+                    while (*p != '}' && *p != '\0') {
+                        putchar(*p);
+                        p++;
+                    }
+                    if (*p == '}') {
+                        p++;
+                    } else {
+                        printf("error: end of assembly not found");
+                    }
+                    while ((*p == '\n') || (*p == '\t') || (*p == ' ')) {
+                        p++;
+                    }
+                    if (strncmp(p, "(asme)", 6) == 0) {
+                        p += 6;
+                    } else {
+                        printf("error: (asme) not found");
+                    }
+                }
+            }
+        } else {
+            printf("error: start of head block not found");
+        }
+    } else {
+        printf("error: head block not found");
+    }
+    *src = p;
 }
