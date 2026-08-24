9a2ab7d32cb833cd8d2834690afda6d9a138857d
falseinfo <norrmanmalte@gmail.com>
2026-08-24 10:12:47 +0200

    Refactor parser_microc for improved string comparison

--- compilermc.c
+++ compilermc.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 #include <string.h>
 
+int MODE_X86_64 = 0;
+
 void parser_microc(const char **src) {
   const char *p = *src;
-  if (strcmp(p, "head", 4) == 0) {
+
+  if (strncmp(p, "head", 4) == 0) {
     p += 4;
 
     if (*p == '(') {
       p++;
-      printf("opened head block");
-      if (strcmp(p, "asm-x86-64", 10) == 0) {
+      printf("opened head block\n");
+
+      if (strncmp(p, "asm-x86-64", 10) == 0) {
         MODE_X86_64 = 1;
         printf("-> enabled x86_64 mode\n");
+        p += 10;
       }
-      while (*p == ')' && *p != '\0') {
+
+      while (*p != ')' && *p != '\0') {
         putchar(*p);
         p++;
       }
+
       if (*p == ')') {  
-        printf("head block closed");
+        printf("\nhead block closed\n");
         p++;
-      } else if {
-        printf("\nerror missing end of head block ")"");
+      } else {
+        printf("\nerror missing end of head block \")\"\n");
       }
-    } else if {
-      printf("\nerror missing start of head block "("");
+    } else {
+      printf("\nerror missing start of head block \"(\"\n");
     }
-  } else if {
-    printf("\nerror missing head block");
+  } else {
+    printf("\nerror missing head block\n");
   }
+
+  *src = p;
 }
 
 int main() {
   const char *code = "head(asm-x86-64)";
-  parse_microc(code);
+  const char *p_code = code;
+  parser_microc(&p_code);
   return 0;
 }
