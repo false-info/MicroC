8172b4ba806a08771a5dbb2487cbad42c0d3c85b
falseinfo <norrmanmalte@gmail.com>
2026-08-24 08:57:07 +0200

    Add head block parser to compilermc.c
    Implement parser for 'head' block in microc.

--- compilermc.c
+++ compilermc.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
-void main() {
-  
+void parser_microc(const char *src) {
+  if (strcmp(p, "head", 4) == 0) {
+    p += 4;
+
+    if (*p == '(') {
+      p++;
+      printf("opened head block");
+      while (*p == ')' && *p != '\0') {
+        putchar(*p);
+        p++;
+      }
+      if (*p == ')') {
+        printf("head block closed");
+        p++;
+      } else if {
+        printf("\nerror missing end of head block ")"");
+      }
+    } else if {
+      printf("\nerror missing start of head block "("");
+    }
+  } else if {
+    printf("\nerror missing head block");
+  }
+}
+
+int main() {
+  const char *code = "head(asm-x86-64)";
+  parse_microc(code);
+  return 0;
 }
