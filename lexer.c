706de4e4fd0444abf53018e64cb599e8423679b2
falseinfo <norrmanmalte@gmail.com>
2026-08-25 21:22:40 +0200

    Refactor lexer to simplify token checks
    Removed redundant checks for 'asm-x86-64' and '(asmb)' while simplifying the lexer logic.

--- lexer.c
+++ lexer.c
@@ -8,35 +8,6 @@ void tokenize(char* source) {
     int length = strlen(source);
     printf("[lexer] start goind thru source code...\n");
     while (i < length) {
-        if (isspace(source[i])) {
-            i++;
-            continue;
-        }
-        if (strncmp(&source[i], "head", 4) == 0) {
-            i += 4;
-            continue;
-        }
-        if (source[i] == '(') {
-            i++;
-            continue;
-        }
-        if (strncmp(&source[i], "asm-x86-64", 10) == 0) {
-            i += 10;
-            continue;
-        }
-        if (source[i] == ')') {
-            i++;
-            continue;
-        }
-        if (source[i] == '{') {
-            i++;
-            continue;
-        }
-        if (source[i] == '{') {
-            i++;
-            continue;
-            
-        }
         if (strncmp(&source[i], "(asmb)", 6) == 0) {
             i += 6;
             printf("found asmb");
@@ -48,6 +19,38 @@ void tokenize(char* source) {
             }
             continue;
         }
+        if (strncmp(&source[i], "asm-x86-64", 10) == 0) {
+            i += 10;
+            continue;
+        }
+        if (strncmp(&source[i], "head", 4) == 0) {
+            i += 4;
+            continue;
+        }        
+        if (isspace(source[i])) {
+            i++;
+            continue;
+        }
+
+        if (source[i] == '(') {
+            i++;
+            continue;
+        }
+
+        if (source[i] == ')') {
+            i++;
+            continue;
+        }
+        if (source[i] == '{') {
+            i++;
+            continue;
+        }
+        if (source[i] == '{') {
+            i++;
+            continue;
+
+        }
+ 
 
     }
 
