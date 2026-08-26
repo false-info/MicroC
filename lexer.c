06361f8e8ce0bd2523fc2332affd4ab69d5c03ad
falseinfo <norrmanmalte@gmail.com>
2026-08-26 23:30:58 +0200

    Implement inline assembly storage in lexer
    Added support for inline assembly storage and error handling.

--- lexer.c
+++ lexer.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
+
+#define ASM_BUFFER_SIZE 65536
+
+char asm_storage[ASM_BUFFER_SIZE];
+int asm_index = 0;
 
 void tokenize(char* source) {
     int i = 0;
@@ -20,7 +26,6 @@ void tokenize(char* source) {
                     i += 6;
                     break;
                 }
-                putchar(source[i]);
                 i++;
             }
             continue;
@@ -39,4 +44,26 @@ void tokenize(char* source) {
         }
         i++;
     }
+}
+
+char* run_asm_vaccum(char *ptr) {
+    ptr += 6;
+    asm_index = 0;
+    asm_storage[0] = '\0';
+    while (*ptr != '\0') {
+        if (strncmp(ptr, "(asme)", 6) == 0) {
+            ptr += 6;
+            break;
+        }
+        if (asm_index < ASM_BUFFER_SIZE - 1) {
+            asm_storage[asm_index] = *ptr;
+            asm_index++;
+        } else {
+            fprintf(stderr, "[compiler-error]: inline-asm block is too big for the memorybuffer\n");
+            exit(1);
+        }
+        ptr++;
+    }
+    asm_storage[asm_index] = '\0';
+    return ptr;
 }
