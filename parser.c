4fae76b0ed18726c34a5b3cb1f28fa557b201b11
falseinfo <norrmanmalte@gmail.com>
2026-08-26 23:29:35 +0200

    Refactor parser_code and add read_word function

--- parser.c
+++ parser.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+char* run_asm_vaccum(char *ptr);
+extern char asm_storage[];
+
 typedef struct Variable {
     char *name;
     int stack_offset;
@@ -35,13 +38,23 @@ Variable* find_variable(char *var_name) {
     return NULL;
 }
 
-void parser_code(char *code) {
-    if (code == NULL) {
-        fprintf(stderr, "[Parser-error]: got a emtpy source-code buffer\n");
-        return;
+void read_word(char *src, char *dest) {
+    int i = 0;
+    while (src[i] != '\0' && (isalnum((unsigned char)src[i]) || src[i] == '_') && i < 31) {
+        dest[i] = src[i];
+        i++;
     }
+    dest[i] = '\0';
+}
+void parser_code(char *code) {
+    if (code == NULL) return;
+
     char *ptr = code;
+    int current_stack_offset = -8;
+    int inside_head = 0;
+    int custom_supported = 0;
     printf("[Parser]: starting parsing of source-code...\n");
+
     while (*ptr != '\0') {
         if (isspace((unsigned char)*ptr)) {
             ptr++;
@@ -50,15 +63,52 @@ void parser_code(char *code) {
         if (strncmp(ptr, "head", 4) == 0) {
             printf("[Parser]: found keyword 'head'\n");
             ptr += 4;
+            if (strstr(ptr, "custom") != NULL) custom_supported = 1;
+            while (*ptr != '{' && *ptr != '\0') ptr++;
+            if (*ptr == '{') {
+                inside_head = 1;
+                ptr++;
+            }
             continue;
         }
-        if (strncmp(ptr, "(asmb)", 6) == 0) {
-            printf("[parser]: found inline-asm (asmb), switching mode...\n");
+        if (inside_head) {
+            if (strncmp(ptr, "(asmb)", 6) == 0) {
+                printf("[parser]: sending to the vaccum...\n");
+                ptr = run_asm_vaccum(ptr);
+                continue;
+            }
 
-            ptr += 6;
-            continue;
+            if (isalpha((unsigned char)*ptr)) {
+                char current_word[32] = {0};
+                read_word(ptr, current_word);
+
+                if (strcmp(current_word, "int") == 0) {
+                    ptr += strlen(current_word);
+                    while (isspace((unsigned char)*ptr)) ptr++;
+                    char var_name[32] = {0};
+                    read_word(ptr, var_name);
+
+                    if (find_variable(var_name) != NULL) {
+                        fprintf(stderr, "[Parser error]: Variable '%s' is already defined\n", var_name);
+                        exit(1);
+                    }
+                    if (!custom_supported) {
+                        fprintf(stderr, "[Parser error]: cannot use 'int' without decalring 'custom' mode in head block\n");
+                        exit(1);
+                    }
+                    add_variable(var_name, current_stack_offset);
+                    current_stack_offset -= 8;
+
+                    ptr += strlen(var_name);
+                    continue;
+                }
+            }
+        }
+        if (*ptr == '}') {
+            inside_head = 0;
+            printf("[Parser]: exiting head block\n");
         }
         ptr++;
     }
-    printf("[Parser]: parsing done\n");
+    printf("\n[Parser]: parsing done\n");
 }
