ea72d46a720a66ee48f7eb24684f31fcc50bef3c
falseinfo <norrmanmalte@gmail.com>
2026-08-27 12:41:36 +0200

    Update parser.c

--- parser.c
+++ parser.c
@@ -16,57 +16,191 @@ typedef struct {
     char text[256];
 } Token;
 
-extern void parse_microc_program(FILE* input_file, FILE* output_file);
-extern void emit_program_epilog(FILE* output_file);
-extern void emit_elf64_header(FILE* output_file);
+extern Token next_token(FILE* input);
+extern int is_valid_identifier(const char* text);
+extern void emit_program_prolog(FILE* out);
+extern int get_or_register_variable(const char* name);
+extern void emit_store_int(int idx, int val, FILE* out);
+extern void emit_load_variable(int idx, FILE* out);
+extern void emit_store_rax_to_variable(int idx, FILE* out);
+extern void emit_add_int(int val, FILE* out);
+extern void emit_sub_int(int val, FILE* out);
+extern void emit_cmp_rax_int(int val, FILE* out);
+extern void emit_cmp_rax_str(const char* str, FILE* out);
+extern long emit_jump_if_not_equal(FILE* out);
+extern long emit_jump_always(FILE* out);
+extern void patch_jump_distance(long patch_pos, long target_pos, FILE* out);
+extern void emit_load_to_rsi(int idx, FILE* out);
+extern void emit_pin_raw_int(int val, FILE* out);
+extern void emit_pin_fmt(const char* fmt, FILE* out);
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <input_file.mc> [-o <output_file>]\n", argv[0]);
-        return 1;
-    }
+void parse_microc_program(FILE* input_file, FILE* output_file) {
+    Token token;
+    int in_asm_mode = 0;
 
-    char* input_filename = argv[1];
-    char* output_filename = "output.bin";
+    while ((token = next_token(input_file)).type != TOKEN_EOF) {
+        
+        if (strcmp(token.text, "head") == 0) {
+            next_token(input_file);
+            next_token(input_file);
+            next_token(input_file);
+            next_token(input_file);
+            next_token(input_file);
+            continue;
+        }
 
-    for (int i = 2; i < argc; i++) {
-        if (strcmp(argv[i], "-o") == 0 && i + 1 < argc) {
-            output_filename = argv[i + 1];
+        if (strcmp(token.text, "(asmb)") == 0) {
+            in_asm_mode = 1;
+            continue;
+        }
+        if (strcmp(token.text, "(asme)") == 0) {
+            in_asm_mode = 0;
+            continue;
+        }
+
+        if (strcmp(token.text, "}") == 0 && !in_asm_mode) {
             break;
         }
+
+        if (in_asm_mode) {
+            if (strcmp(token.text, "cli") == 0) {
+                fputc(0xFA, output_file);
+            } else if (strcmp(token.text, "hlt") == 0) {
+                fputc(0xF4, output_file);
+            } else if (strcmp(token.text, "pad_boot") == 0) {
+                long pos = ftell(output_file);
+                while (pos < 510) {
+                    fputc(0x00, output_file);
+                    pos++;
+                }
+            } else if (strcmp(token.text, "sign_boot") == 0) {
+                fputc(0x55, output_file);
+                fputc(0xAA, output_file);
+            }
+        } 
+        else {
+            if (strcmp(token.text, "fn") == 0) {
+                Token fn_name = next_token(input_file);
+                next_token(input_file);
+                next_token(input_file);
+                next_token(input_file);
+                emit_program_prolog(output_file);
+            }
+            else if (strcmp(token.text, "if") == 0) {
+                next_token(input_file);
+                Token left = next_token(input_file);
+                next_token(input_file);
+                Token right = next_token(input_file);
+                next_token(input_file);
+                next_token(input_file);
+                
+                if (strcmp(left.text, "current") == 0) {
+                    if (right.type == TOKEN_STRING) {
+                        emit_cmp_rax_str(right.text, output_file);
+                    } else if (right.type == TOKEN_NUMBER) {
+                        emit_cmp_rax_int(atoi(right.text), output_file);
+                    }
+                } else {
+                    int idx = get_or_register_variable(left.text);
+                    emit_load_variable(idx, output_file);
+                    emit_cmp_rax_int(atoi(right.text), output_file);
+                }
+                
+                long patch_pos = emit_jump_if_not_equal(output_file);
+                
+                parse_microc_program(input_file, output_file);
+                
+                long end_pos = ftell(output_file);
+                patch_jump_distance(patch_pos, end_pos, output_file);
+            }
+            else if (strcmp(token.text, "while") == 0) {
+                next_token(input_file);
+                Token left = next_token(input_file);
+                next_token(input_file);
+                Token right = next_token(input_file);
+                next_token(input_file);
+                next_token(input_file);
+                
+                long loop_start = ftell(output_file);
+                
+                if (strcmp(left.text, "current") == 0) {
+                    if (right.type == TOKEN_STRING) {
+                        emit_cmp_rax_str(right.text, output_file);
+                    } else if (right.type == TOKEN_NUMBER) {
+                        emit_cmp_rax_int(atoi(right.text), output_file);
+                    }
+                } else {
+                    int idx = get_or_register_variable(left.text);
+                    emit_load_variable(idx, output_file);
+                    emit_cmp_rax_int(atoi(right.text), output_file);
+                }
+                
+                long patch_pos = emit_jump_if_not_equal(output_file);
+                
+                parse_microc_program(input_file, output_file);
+                
+                long jump_back_pos = emit_jump_always(output_file);
+                patch_jump_distance(jump_back_pos, loop_start, output_file);
+                
+                long end_pos = ftell(output_file);
+                patch_jump_distance(patch_pos, end_pos, output_file);
+            }
+            else if (strcmp(token.text, "pin") == 0) {
+                next_token(input_file);
+                Token format_tok = next_token(input_file);
+                
+                if (format_tok.type == TOKEN_NUMBER) {
+                    emit_pin_raw_int(atoi(format_tok.text), output_file);
+                    next_token(input_file);
+                } 
+                else if (format_tok.type == TOKEN_STRING) {
+                    Token comma_or_close = next_token(input_file);
+                    if (strcmp(comma_or_close.text, ",") == 0) {
+                        Token arg_tok = next_token(input_file);
+                        if (arg_tok.type == TOKEN_IDENTIFIER) {
+                            int idx = get_or_register_variable(arg_tok.text);
+                            emit_load_to_rsi(idx, output_file);
+                        }
+                        emit_pin_fmt(format_tok.text, output_file);
+                        next_token(input_file);
+                    } else {
+                        emit_pin_fmt(format_tok.text, output_file);
+                    }
+                }
+            }
+            else if (strcmp(token.text, "i64") == 0) {
+                Token var_name = next_token(input_file);
+                next_token(input_file);
+                Token val = next_token(input_file);
+                
+                int idx = get_or_register_variable(var_name.text);
+                emit_store_int(idx, atoi(val.text), output_file);
+            }
+            else if (is_valid_identifier(token.text)) {
+                char var_name[256];
+                strcpy(var_name, token.text);
+                Token op = next_token(input_file);
+                
+                if (strcmp(op.text, "=") == 0) {
+                    Token val = next_token(input_file);
+                    int idx = get_or_register_variable(var_name);
+                    
+                    Token next_op = next_token(input_file);
+                    if (strcmp(next_op.text, "+") == 0) {
+                        Token right_num = next_token(input_file);
+                        emit_load_variable(idx, output_file);
+                        emit_add_int(atoi(right_num.text), output_file);
+                        emit_store_rax_to_variable(idx, output_file);
+                    } else if (strcmp(next_op.text, "-") == 0) {
+                        Token right_num = next_token(input_file);
+                        emit_load_variable(idx, output_file);
+                        emit_sub_int(atoi(right_num.text), output_file);
+                        emit_store_rax_to_variable(idx, output_file);
+                    } else {
+                        emit_store_int(idx, atoi(val.text), output_file);
+                    }
+                }
+            }
+        }
     }
-
-    FILE* input_file = fopen(input_filename, "r");
-    if (!input_file) {
-        printf("Error: Could not open input file '%s'\n", input_filename);
-        return 1;
-    }
-
-    FILE* output_file = fopen(output_filename, "wb");
-    if (!output_file) {
-        printf("Error: Could not create output file '%s'\n", output_filename);
-        fclose(input_file);
-        return 1;
-    }
-
-    int is_bin = 0;
-    int len = strlen(output_filename);
-    if (len >= 4 && strcmp(&output_filename[len - 4], ".bin") == 0) {
-        is_bin = 1;
-    }
-
-    if (!is_bin) {
-        emit_elf64_header(output_file);
-    }
-
-    parse_microc_program(input_file, output_file);
-
-    if (!is_bin) {
-        emit_program_epilog(output_file);
-    }
-
-    fclose(input_file);
-    fclose(output_file);
-
-    return 0;
 }
