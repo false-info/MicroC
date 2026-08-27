e793797531e41b959d7f2b28715956ac01cff19d
falseinfo <norrmanmalte@gmail.com>
2026-08-28 00:46:17 +0200

    Update lexer.c

--- lexer.c
+++ lexer.c
@@ -16,6 +16,436 @@ typedef struct {
     char text[256];
 } Token;
 
+int is_valid_identifier(const char* text)
+{
+    if (!text || !text[0])
+        return 0;
+
+    if (!isalpha((unsigned char)text[0]) &&
+        text[0] != '_')
+        return 0;
+
+    for (int i = 1; text[i] != '\0'; i++) {
+        if (!isalnum((unsigned char)text[i]) &&
+            text[i] != '_')
+            return 0;
+    }
+
+    return 1;
+}
+
+static int is_keyword(const char* text)
+{
+    static const char* words[] = {
+        "head",
+        "i64",
+        "fn",
+        "return",
+        "if",
+        "while",
+        "pin",
+        "custom",
+        "asm-x86-64"
+    };
+
+    size_t count =
+        sizeof(words) / sizeof(words[0]);
+
+    for (size_t i = 0; i < count; i++) {
+        if (strcmp(text, words[i]) == 0)
+            return 1;
+    }
+
+    return 0;
+}
+
+static Token make_token(
+    TokenType type,
+    const char* text
+)
+{
+    Token token;
+
+    memset(
+        &token,
+        0,
+        sizeof(token)
+    );
+
+    token.type = type;
+
+    if (text) {
+        size_t length =
+            strlen(text);
+
+        if (length >= sizeof(token.text))
+            length =
+                sizeof(token.text) - 1;
+
+        memcpy(
+            token.text,
+            text,
+            length
+        );
+
+        token.text[length] = '\0';
+    }
+
+    return token;
+}
+
+Token next_token(FILE* input)
+{
+    int c;
+
+    while (1) {
+        c = fgetc(input);
+
+        if (c == EOF)
+            return make_token(
+                TOKEN_EOF,
+                "EOF"
+            );
+
+        if (isspace((unsigned char)c))
+            continue;
+
+        if (c == '/') {
+            int next =
+                fgetc(input);
+
+            if (next == '/') {
+                while ((c = fgetc(input)) != EOF &&
+                       c != '\n')
+                    ;
+
+                continue;
+            }
+
+            if (next != EOF)
+                ungetc(next, input);
+        }
+
+        break;
+    }
+
+    if (c == '"') {
+        char buffer[256];
+        int index = 0;
+
+        while (1) {
+            c = fgetc(input);
+
+            if (c == EOF ||
+                c == '"')
+                break;
+
+            if (c == '\\') {
+                int next =
+                    fgetc(input);
+
+                if (next == 'n')
+                    c = '\n';
+                else if (next == 'r')
+                    c = '\r';
+                else if (next == 't')
+                    c = '\t';
+                else if (next == '\\')
+                    c = '\\';
+                else if (next == '"')
+                    c = '"';
+                else
+                    c = next;
+            }
+
+            if (index < 255)
+                buffer[index++] =
+                    (char)c;
+        }
+
+        buffer[index] = '\0';
+
+        return make_token(
+            TOKEN_STRING,
+            buffer
+        );
+    }
+
+    if (c == '(') {
+        char buffer[16];
+        int index = 0;
+
+        buffer[index++] =
+            '(';
+
+        int next =
+            fgetc(input);
+
+        while (next != EOF &&
+               next != ')' &&
+               index < 15) {
+
+            buffer[index++] =
+                (char)next;
+
+            next =
+                fgetc(input);
+        }
+
+        if (next == ')') {
+            buffer[index++] =
+                ')';
+
+            buffer[index] =
+                '\0';
+
+            if (!strcmp(
+                    buffer,
+                    "(asmb)"
+                ) ||
+                !strcmp(
+                    buffer,
+                    "(asme)"
+                )) {
+
+                return make_token(
+                    TOKEN_SYMBOL,
+                    buffer
+                );
+            }
+
+            for (int i = index - 1;
+                 i >= 1;
+                 i--) {
+
+                ungetc(
+                    (unsigned char)buffer[i],
+                    input
+                );
+            }
+        }
+        else {
+            if (next != EOF)
+                ungetc(
+                    next,
+                    input
+                );
+
+            for (int i = index - 1;
+                 i >= 1;
+                 i--) {
+
+                ungetc(
+                    (unsigned char)buffer[i],
+                    input
+                );
+            }
+        }
+
+        return make_token(
+            TOKEN_SYMBOL,
+            "("
+        );
+    }
+
+    if (c == '=' ||
+        c == '!' ||
+        c == '<' ||
+        c == '>') {
+
+        char buffer[3];
+
+        buffer[0] =
+            (char)c;
+
+        buffer[1] =
+            '\0';
+
+        buffer[2] =
+            '\0';
+
+        int next =
+            fgetc(input);
+
+        if (next == '=') {
+            buffer[1] =
+                '=';
+        }
+        else if (c == '<' &&
+                 next == '<') {
+
+            buffer[1] =
+                '<';
+        }
+        else if (c == '>' &&
+                 next == '>') {
+
+            buffer[1] =
+                '>';
+        }
+        else if (next != EOF) {
+            ungetc(
+                next,
+                input
+            );
+        }
+
+        return make_token(
+            TOKEN_SYMBOL,
+            buffer
+        );
+    }
+
+    if (strchr(
+            "){},[]+-*/%&|^",
+            c
+        )) {
+
+        char buffer[2];
+
+        buffer[0] =
+            (char)c;
+
+        buffer[1] =
+            '\0';
+
+        return make_token(
+            TOKEN_SYMBOL,
+            buffer
+        );
+    }
+
+    if (isdigit((unsigned char)c)) {
+        char buffer[256];
+        int index = 0;
+
+        buffer[index++] =
+            (char)c;
+
+        if (c == '0') {
+            int next =
+                fgetc(input);
+
+            if (next == 'x' ||
+                next == 'X') {
+
+                buffer[index++] =
+                    (char)next;
+
+                while ((c = fgetc(input)) != EOF &&
+                       isxdigit((unsigned char)c)) {
+
+                    if (index < 255)
+                        buffer[index++] =
+                            (char)c;
+                }
+
+                if (c != EOF)
+                    ungetc(
+                        c,
+                        input
+                    );
+
+                buffer[index] =
+                    '\0';
+
+                return make_token(
+                    TOKEN_NUMBER,
+                    buffer
+                );
+            }
+
+            if (next != EOF)
+                ungetc(
+                    next,
+                    input
+                );
+        }
+
+        while ((c = fgetc(input)) != EOF &&
+               isdigit((unsigned char)c)) {
+
+            if (index < 255)
+                buffer[index++] =
+                    (char)c;
+        }
+
+        if (c != EOF)
+            ungetc(
+                c,
+                input
+            );
+
+        buffer[index] =
+            '\0';
+
+        return make_token(
+            TOKEN_NUMBER,
+            buffer
+        );
+    }
+
+    if (isalpha((unsigned char)c) ||
+        c == '_') {
+
+        char buffer[256];
+        int index = 0;
+
+        while ((isalnum((unsigned char)c) ||
+                c == '_' ||
+                c == '-') &&
+               index < 255) {
+
+            buffer[index++] =
+                (char)c;
+
+            c =
+                fgetc(input);
+        }
+
+        if (c != EOF)
+            ungetc(
+                c,
+                input
+            );
+
+        buffer[index] =
+            '\0';
+
+        if (is_keyword(buffer))
+            return make_token(
+                TOKEN_KEYWORD,
+                buffer
+            );
+
+        return make_token(
+            TOKEN_IDENTIFIER,
+            buffer
+        );
+    }
+
+    return make_token(
+        TOKEN_SYMBOL,
+        (char[]){(char)c, '\0'}
+    );
+}#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+typedef enum {
+    TOKEN_KEYWORD,
+    TOKEN_IDENTIFIER,
+    TOKEN_NUMBER,
+    TOKEN_STRING,
+    TOKEN_SYMBOL,
+    TOKEN_EOF
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char text[256];
+} Token;
+
 int is_valid_identifier(const char* text) {
     if (!text || !text[0])
         return 0;
